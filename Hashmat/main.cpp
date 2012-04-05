ab32162a30342a43277576763ff21bdae311e3ff
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-05 16:26:53 -0300

    Changed to long int

--- Hashmat/main.cpp
+++ Hashmat/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
 int main () {
-  int x, y;
+  long int x, y;
   while (!cin.eof()) {
     cin >> x >> y;
     if (cin.eof())
       break;
-    cout << fabs(y - x) << endl;
+    cout << setiosflags(ios::fixed) <<
+      setprecision(0) << fabs(y - x) << endl;
   }
   return 0;
 }
