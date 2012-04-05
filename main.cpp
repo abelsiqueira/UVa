a9536105554bf3b2dd6920564b67adb58ec39918
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-05 11:29:43 -0300

    Created new functions

--- main.cpp
+++ main.cpp
@@ -38,9 +38,38 @@ class Vector3 {
       for (size_t i = 0; i < 3; i++)
         v[i] += alpha*P.get(i);
     }
+    void cross (const Vector3 & P, const Vector3 & Q) {
+      v[0] = P.get(1)*Q.get(2) - P.get(2)*Q.get(1);
+      v[1] = P.get(2)*Q.get(0) - P.get(0)*Q.get(2);
+      v[2] = P.get(0)*Q.get(1) - P.get(1)*Q.get(0);
+    }
+    float norm () const {
+      float n = 0;
+      for (size_t i = 0; i < 3; i++)
+        n += v[i]*v[i];
+      return sqrt(n);
+    }
+    float sqrnorm () const {
+      float n = 0;
+      for (size_t i = 0; i < 3; i++)
+        n += v[i]*v[i];
+      return n;
+    }
+    float dot (const Vector3 & P) const {
+      float d = 0.0;
+      for (size_t i = 0; i < 3; i++)
+        d += v[i]*P.get(i);
+      return d;
+    }
   private:
     int v[3];
 };
+
+float distance_to_plane (const Vector3 & normal, const Vector3 & planePoint, 
+    const Vector3 & point) {
+  float d = fabs( normal.dot(planePoint) - normal.dot(point) );
+  return d/normal.norm();
+}
 
 float upper_bound (Vector3 *P, Vector3 *Q) {
   float d = 1e9, t = 0.0;
