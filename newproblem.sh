07eadeae4d3dc5b044781f9212b9b2ae716ff6ab
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-05 16:41:28 -0300

    Created newproblem.sh
    This script creates a directory with the basic files to
    start a new problem

---
+++ newproblem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+problem=$1
+[[ "$#" -eq 1 ]] || { echo "1 argument required"; exit 1;}
+mkdir "$problem"
+touch "$problem"/STATUS
+echo $problem >> "$problem"/STATUS
+cp templates/* "$problem"
