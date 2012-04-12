bbb320085c90345a4371a49739f391c0dbbf085c
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-12 01:39:54 -0300

    New Star War test

--- Star War/main.cpp
+++ Star War/main.cpp
@@ -21,6 +21,26 @@ class Vector3 {
       for (size_t i = 0; i < 3; i++)
         v[i] = P.get(i);
     }
+    Vector3 operator+(const Vector3 & P) {
+      Vector3 Q(P);
+      Q.axpy(1,*this);
+      return P;
+    }
+    Vector3 operator-(const Vector3 & P) {
+      Vector3 Q(*this);
+      Q.axpy(-1,P);
+    }
+    Vector3 plus (double a) {
+      Vector3 Q(*this);
+      for (size_t i = 0; i < 3; i++)
+        Q.v[i] *= a;
+    }
+    double sum () {
+      double a = 0;
+      for (size_t i = 0; i < 3; i++)
+        a += v[i];
+      return a;
+    }
 
     void print () {
       for (size_t i = 0; i < 3; i++)
@@ -152,21 +172,138 @@ float calc_sqrdist(Vector3 *P, Vector3 *Q, const Vector3 & a, const Vector3 & b)
   return distance.sqrnorm();
 }
 
-float solve (Vector3 *P, Vector3 *Q) {
-  Vector3 a(0,0,0), b(0,0,0);
-  enum directions {e1, e2, e3, e4, e5, e6};
-  directions best_direction;
-  float step = 1, minStep = pow(2,-3);
-  float dist = 1e9, temp = 0.0;
-
-  while (step > minStep) {
-    bool found_direction = false;
-
-    for (size_t d = 0; d < 3; d++) {
-    }
-    if (!found_direction)
-      step /= 2.0;
+float projs (Vector3 *P, Vector3 *Q) {
+  Vector3 c1(0,0,0), c2(0,0,0);
+  float d = 1e9, t = 0.0;
+  for (size_t i = 0; i < 4; i++) {
+    c1.axpy(0.25, P[i]);
+    c2.axpy(0.25, Q[i]);
   }
+  Vector3 dist(c2);
+  dist.axpy(-1,c1);
+
+  for (size_t i = 0; i < 4; i++) {
+    P[i].axpy(-1, c1);
+    Q[i].axpy(-1, c2);
+  }
+
+  bool Pbelongs[4], Qbelongs[4];
+  size_t Ps = 0, Qs = 0;
+  for (size_t i = 0; i < 4; i++) {
+    if (dist.dot(P[i]) > 0) {
+      Pbelongs[i] = true;
+      Ps++;
+    } else
+      Pbelongs[i] = false;
+    if (dist.dot(Q[i]) < 0) {
+      Qbelongs[i] = true;
+      Qs++;
+    } else
+      Qbelongs[i] = false;
+  }
+  for (size_t i = 0; i < 4; i++) {
+    P[i].axpy(1, c1);
+    Q[i].axpy(1, c2);
+  }
+//  if ( (Ps == 1) && (Qs == 1) ) {
+    for (size_t i = 0; i < 4; i++) {
+      for (size_t j = 0; j < 4; j++) {
+        if (Pbelongs[i] && Qbelongs[j]) {
+          t = P[i].sqrdist(Q[j]);
+          if (t < d)
+            d = t;
+        }
+      }
+    }
+//  }
+  cout << "Ps = " << Ps << endl;
+  cout << "Qs = " << Qs << endl;
+
+  return sqrt(d);
+}
+
+float solve (Vector3 *P, Vector3 *Q) {
+  Vector3 *SDs;
+  SDs = new Vector3[8];
+  SDs[0].set(1, 0, 0);
+  SDs[1].set(0, 1, 0);
+  SDs[2].set(0, 0, 1);
+  SDs[3].set(-1, 0, 0);
+  SDs[4].set(0, -1, 0);
+  SDs[5].set(0, 0, -1);
+  SDs[6].set(-1, -1, 0);
+  SDs[7].set(-1, 0, -1);
+  SDs[8].set(0, -1, -1);
+  for (size_t i = 1; i < 4; i++) {
+    P[i].axpy(-1, P[0]);
+    Q[i].axpy(-1, P[0]);
+  }
+  size_t n = 8;
+  double step=1.0;
+  double d=1e9, t=0.0;
+  Vector3 a(0,0,0), b(0,0,0);
+  Vector3 direction(0,0,0);
+
+//  for (size_t i = 0; i < 3; i++)
+//    cout << a.get(i) << ",";
+//  for (size_t i = 0; i < 3; i++)
+//    cout << b.get(i) << ",";
+//  cout << endl;
+  while (step > 2e-9) {
+    bool decreased = false;
+    for (size_t i = 0; i < n; i++) {
+      a.axpy(step,SDs[i]);
+      if ( (a.get(0) < 0) || (a.get(1) < 0) || (a.get(2) < 0) || (a.sum() > 1) ) {
+        a.axpy(-step,SDs[i]);
+        continue;
+      }
+      direction = P[0];
+      direction.axpy(-1,Q[0]);
+//      direction = P[0] - Q[0];
+      for (size_t j = 0; j < 3; j++) {
+        direction.axpy(a.get(j), P[j+1]);
+        direction.axpy(-b.get(j), Q[j+1]);
+      }
+      t = direction.sqrnorm();
+      if (t < d) {
+        d = t;
+        decreased = true;
+        break;
+      } else
+        a.axpy(-step,SDs[i]);
+    }
+    for (size_t i = 0; decreased == false, i < n; i++) {
+      b.axpy(step,SDs[i]);
+      if ( (b.get(0) < 0) || (b.get(1) < 0) || (b.get(2) < 0) || (b.sum() > 1) ) {
+        b.axpy(-step,SDs[i]);
+        continue;
+      }
+      direction = P[0];
+      direction.axpy(-1,Q[0]);
+//      direction = P[0] - Q[0];
+      for (size_t j = 0; j < 3; j++) {
+        direction.axpy(a.get(j), P[j+1]);
+        direction.axpy(-b.get(j), Q[j+1]);
+      }
+      t = direction.sqrnorm();
+      if (t < d) {
+        d = t;
+        decreased = true;
+        break;
+      } else 
+        b.axpy(-step,SDs[i]);
+    }
+    if (!decreased)
+      step /= 2;
+    else {
+//      for (size_t i = 0; i < 3; i++)
+//        cout << a.get(i) << ",";
+//      for (size_t i = 0; i < 3; i++)
+//        cout << b.get(i) << ",";
+//      cout << endl;
+    }
+  }
+  return sqrt(d);
 }
 
 int main () {
