d3a048ea64aaf0e60e0432f8a18c1344b947b416
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-16 00:46:01 -0300

    190 Completed

--- 190 - Circle/main.cpp
+++ 190 - Circle/main.cpp
@@ -12,18 +12,20 @@ int main () {
     cin >> Ax >> Ay >> Bx >> By >> Cx >> Cy;
     if (cin.eof())
       break;
-    Mx = (Ax + Bx)/2;
-    My = (Ay + By)/2;
-    Nx = (Ax + Cx)/2;
-    Ny = (Ay + Cy)/2;
+    Mx = (Ax + Bx)/2.0;
+    My = (Ay + By)/2.0;
+    Nx = (Ax + Cx)/2.0;
+    Ny = (Ay + Cy)/2.0;
     mM = Ay - By;
     mN = Ay - Cy;
     if (mM == 0) {
-      x = Nx;
-      y = My + mM*(x - Mx);
-    } else if (mN == 0) {
       x = Mx;
+      mN = -(Ax - Cx)/mN;
       y = Ny + mN*(x - Nx);
+    } else if (mN == 0) {
+      x = Nx;
+      mM = -(Ax - Bx)/mM;
+      y = My + mM*(x - Mx);
     } else {
       mM = -(Ax - Bx)/mM;
       mN = -(Ax - Cx)/mN;
@@ -33,28 +35,34 @@ int main () {
     R = sqrt( pow(Ax - x,2) + pow(Ay - y,2));
     e = x*x + y*y - R*R;
 
-    if (x >= 0)
+    if (x > 1e-4)
       printf("(x - %0.3lf)^2 + ", x);
-    else 
+    else if (x < -1e-4)
       printf("(x + %0.3lf)^2 + ", -x);
-    if (y >= 0)
+    else 
+      printf("x^2 + ");
+    if (y > 1e-4)
       printf("(y - %0.3lf)^2 = %0.3lf^2\n", y, R);
-    else
+    else if (y < -1e-4)
       printf("(y + %0.3lf)^2 = %0.3lf^2\n", -y, R);
+    else
+      printf("y^2 = %0.3lf^2\n", R);
 
     cout << "x^2 + y^2 ";
-    if (x >= 0)
+    if (x > 1e-4)
       printf("- %0.3lfx ", 2*x);
-    else
+    else if (x < -1e-4)
       printf("+ %0.3lfx ", -2*x);
-    if (y >= 0)
+    if (y > 1e-4)
       printf("- %0.3lfy ", 2*y);
-    else
+    else if (y < -1e-4)
       printf("+ %0.3lfy ", -2*y);
-    if (e >= 0)
+    if (e > 1e-4)
       printf("+ %0.3lf = 0\n\n", e);
-    else
+    else if (e < -1e-4)
       printf("- %0.3lf = 0\n\n", -e);
+    else
+      printf("= 0\n\n");
   }
   return 0;
 }
