2afd89014f32c1a2aab6407e332b0de2f36ed619
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-04-18 09:43:08 -0300

    Started and tried Peter Smokes

---
+++ Peter's Smokes/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+  size_t n, k, d, r, cigars;
+  while (!cin.eof()) {
+    cin >> n >> k;
+    if (cin.eof())
+      break;
+    cigars = n;
+    d = n/k;
+    r = n - d*k;
+    n = d;
+    while (n > 0) {
+      cigars += n;
+      d = (n + r)/k;
+      r = n - d*k;
+      n = d;
+    }
+    cout << cigars << endl;
+  }
+  return 0;
+}
